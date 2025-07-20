3e72508f1224c8484c6099ea0e573a1052afcb2e
NeKon69 <nobodqwe@gmail.com>
2025-07-20 18:39:02 +0300

    added stream class

---
+++ include/cuda_types/stream.h
@@ -0,0 +1,29 @@
+//
+// Created by progamers on 7/20/25.
+//
+
+#ifndef SPACE_EXPLORER_STREAM_H
+#define SPACE_EXPLORER_STREAM_H
+#include <cuda_runtime.h>
+#include <raw_memory.h>
+namespace raw {
+class cuda_stream {
+private:
+	cudaStream_t		  _stream;
+	raw::shared_ptr<bool> created;
+
+public:
+	cuda_stream();
+	cuda_stream(const cuda_stream& rhs) = default;
+	cuda_stream(cuda_stream&& rhs) noexcept;
+	cuda_stream& operator=(const cuda_stream& rhs);
+	cuda_stream& operator=(cuda_stream&& rhs) noexcept;
+	void		 sync();
+	void		 destroy();
+	void		 create();
+	cudaStream_t stream();
+	~cuda_stream();
+};
+} // namespace raw
+
+#endif // SPACE_EXPLORER_STREAM_H
---
+++ src/cuda_data_types/stream.cpp
@@ -0,0 +1,53 @@
+//
+// Created by progamers on 7/20/25.
+//
+#include "cuda_types/stream.h"
+
+#include "helper_macros.h"
+namespace raw {
+cuda_stream::cuda_stream() : created(make_shared<bool>(false)) {
+	CUDA_SAFE_CALL(cudaStreamCreate(&_stream));
+	*created = true;
+}
+void cuda_stream::destroy() {
+	if (created)
+		CUDA_SAFE_CALL(cudaStreamDestroy(_stream));
+	*created = false;
+}
+void cuda_stream::create() {
+	if (!created)
+		CUDA_SAFE_CALL(cudaStreamCreate(&_stream));
+	*created = true;
+}
+void cuda_stream::sync() {
+	CUDA_SAFE_CALL(cudaStreamSynchronize(_stream));
+}
+cuda_stream::cuda_stream(raw::cuda_stream &&rhs) noexcept
+	: _stream(rhs._stream), created(std::move(rhs.created)) {
+	rhs._stream	 = nullptr;
+	*rhs.created = false;
+}
+cuda_stream &cuda_stream::operator=(const raw::cuda_stream &rhs) {
+	destroy();
+	_stream = rhs._stream;
+	created = rhs.created;
+	return *this;
+}
+cuda_stream &cuda_stream::operator=(raw::cuda_stream &&rhs) noexcept {
+	destroy();
+	_stream		= rhs._stream;
+	created		= rhs.created;
+	rhs._stream = nullptr;
+	rhs.created = nullptr;
+	return *this;
+}
+
+cudaStream_t cuda_stream::stream() {
+	return _stream;
+}
+
+cuda_stream::~cuda_stream() {
+	if(created)
+    cudaStreamDestroy(_stream);
+}
+} // namespace raw
