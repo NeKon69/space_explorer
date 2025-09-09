18bc69e2cc7f8254f80a6c228132c6c6d50e22a2
NeKon69 <nobodqwe@gmail.com>
2025-09-09 21:17:30 +0300

    now the window opened and everyhting compiled, but gotta debug n-body tomorrow

--- src/shaders/objects/vertex_shader.glsl
+++ src/shaders/objects/vertex_shader.glsl
@@ -1,4 +1,5 @@
 #version 410 core
+#extension GL_NV_gpu_shader5 : enable
 layout(location = 0) in vec3 a_pos;
 layout(location = 1) in vec3 a_normal;
 layout(location = 2) in vec2 a_tex_coord;
@@ -6,8 +7,8 @@ layout(location = 3) in vec3 a_tangent;
 layout(location = 4) in vec3 a_bitangent;
 // Instancing (one for each instance)
 layout(location = 5) in mat4 a_model;
-layout(location = 5 + 4) in uint64_t texture_location1;
-layout(location = 5 + 4 + 1) in uint64_t texture_location2;
+layout(location = 9) in uint64_t texture_location1;
+layout(location = 10) in uint64_t texture_location2;
 
 uniform mat4 view;
 uniform mat4 projection;
